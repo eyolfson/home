0072dd8aa24931c0416ba4005a8dba39daa13d5f
Jon Eyolfson <jon@eyolfson.com>
2010-01-19 22:57:31 -0500

    Fixed emacs init for yasnippet.

--- emacs/init.el
+++ emacs/init.el
@@ -10,8 +10,7 @@
         (normal-top-level-add-subdirs-to-load-path)))
 
 ;;==============================================================================
-;; Color Theme 6.6.0
-;; http://www.nongnu.org/color-theme/
+;; Color Theme
 ;;
 ;; Color Theme Tango 2
 ;; 05ed82ef54947c8830e77c147a57f9325e833378
@@ -24,28 +23,13 @@
 (eval-after-load "color-theme" '(color-theme-tango-2))
 
 ;;==============================================================================
-;; Haskell Mode 2.7.0
-;; http://projects.haskell.org/haskellmode-emacs/
+;; Haskell Mode
 ;;==============================================================================
-;;(load-library "haskell-site-file")
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 
 ;;==============================================================================
-;; Python Mode 5.1.0
-;; https://launchpad.net/python-mode/
+;; YASnippet
 ;;==============================================================================
-;;(autoload 'python-mode "python-mode" "Python Mode." t)
-;;(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
-;;(add-to-list 'interpreter-mode-alist '("python" . python-mode))
-;;(add-hook 'python-mode-hook
-;;    (lambda ()
-;;        (set (make-variable-buffer-local 'beginning-of-defun-function)
-;;            'py-beginning-of-def-or-class)
-;;        (setq outline-regexp "def\\|class ")))
-
-;;==============================================================================
-;; YASnippet 0.6.1c
-;; http://code.google.com/p/yasnippet/
-;;==============================================================================
-;;(require 'yasnippet)
-;;(yas/initialize)
-;;(yas/load-directory "~/.emacs.d/site-lisp/yasnippet/snippets")
+(yas/initialize)
+(yas/load-directory "/usr/share/emacs/etc/yasnippet/snippets")
