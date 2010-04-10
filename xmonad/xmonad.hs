7b92e463b407a97b5cbe2edf345cb4e4f1dbd20b
Jon Eyolfson <jon@eyolfson.com>
2010-04-09 21:41:59 -0400

    XMonad config updated to use xmonad-log-applet.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,42 +1,83 @@
+import Control.OldException
+import Control.Monad
+import DBus
+import DBus.Connection
+import DBus.Message
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Config.Gnome
 import XMonad.Util.EZConfig (additionalKeysP)
-import XMonad.Util.Run (spawnPipe, hPutStrLn)
 import XMonad.Layout.NoBorders(smartBorders)
 
-myStatusBar = "/home/jon/bin/dzen2 -xs 1 -e 'onstart=lower' -w 1170 -h 18 -ta l -fg '#ffffff' -bg '#000000'"
+-- This retry is really awkward, but sometimes DBus won't let us get our
+-- name unless we retry a couple times.
+getWellKnownName :: Connection -> IO ()
+getWellKnownName dbus = tryGetName `catchDyn` (\ (DBus.Error _ _) ->
+                                                getWellKnownName dbus)
+ where
+  tryGetName = do
+    namereq <- newMethodCall serviceDBus pathDBus interfaceDBus "RequestName"
+    addArgs namereq [String "org.xmonad.Log", Word32 5]
+    sendWithReplyAndBlock dbus namereq 0
+    return ()
 
-main = do
-       din <- spawnPipe myStatusBar
-       xmonad $ gnomeConfig
+
+main :: IO ()
+main = withConnection Session $ \ dbus -> do
+  getWellKnownName dbus
+  xmonad $ gnomeConfig
            { modMask = mod4Mask
            , terminal = "gnome-terminal"
            , workspaces = ["Web", "Term", "Emacs", "IM", "IRC", "Misc", "Pres"]
-           , logHook = dynamicLogWithPP $ myPP din
+           , logHook = dynamicLogWithPP $ myPP dbus
            , layoutHook = smartBorders $ layoutHook gnomeConfig 
            }
-           `additionalKeysP` myKeys din
+           `additionalKeysP` myKeys
 
-myPP h = defaultPP
-             { ppCurrent = wrap "^fg(#000000)^bg(#09acda) " " ^fg()^bg()"
-             , ppHidden = wrap " " " "
-             , ppHiddenNoWindows = wrap " " " "
-             , ppSep = ""
-             , ppWsSep = ""
-             , ppLayout = wrap " ^ro(8x8) " " "
-             , ppTitle = \_ -> ""
-             , ppOutput = hPutStrLn h
+myPP dbus = defaultPP
+             { ppCurrent  = pangoColor "#ad7fa8" . wrap "[" "]"
+                            -- , ppHiddenNoWindows = wrap " " " "
+                            -- , ppSep = ""
+                            -- , ppWsSep = ""
+
+             , ppTitle    = pangoColor "#729fcf" . shorten 50
+             , ppVisible  = pangoColor "#663366" . wrap "(" ")"
+             , ppHidden   = wrap " " " "
+             , ppUrgent   = pangoColor "red"
+             , ppOutput   = \ str -> do
+               let str'  = "<span font=\"Terminus 9 Bold\">" ++ str ++ 
+                           "</span>"
+                   str'' = sanitize str'
+               msg <- newSignal "/org/xmonad/Log" "org.xmonad.Log" 
+                      "Update"
+               addArgs msg [String str'']
+               -- If the send fails, ignore it.
+               send dbus msg 0 `catchDyn`
+                 (\ (DBus.Error _name _msg) ->
+                   return 0)
+               return ()
              }
 
-myKeys conf = [ ("M-f", spawn "firefox")
-              , ("M-x", spawn "gnome-terminal")
-              , ("M-g", spawn "emacs")
-              , ("M-d", spawn "pidgin")
-              , ("M-s", spawn "nautilus ~")
-              , ("M-a", spawn "vlc")
-              , ("M-z", spawn "evince")
-              , ("M-[", spawn "mpc --no-status prev")
-              , ("M-]", spawn "mpc --no-status next")
-              , ("M-\\", spawn "mpc --no-status toggle")
-              ]
+myKeys = [ ("M-f", spawn "firefox")
+         , ("M-x", spawn "gnome-terminal")
+         , ("M-g", spawn "emacs")
+         , ("M-d", spawn "pidgin")
+         , ("M-s", spawn "nautilus ~")
+         , ("M-a", spawn "vlc")
+         , ("M-z", spawn "evince")
+         , ("M-[", spawn "mpc --no-status prev")
+         , ("M-]", spawn "mpc --no-status next")
+         , ("M-\\", spawn "mpc --no-status toggle")
+         ]
+
+pangoColor :: String -> String -> String
+pangoColor fg = wrap left right
+ where
+  left  = "<span foreground=\"" ++ fg ++ "\">"
+  right = "</span>"
+
+sanitize :: String -> String
+sanitize [] = []
+sanitize (x:rest) | fromEnum x > 127 = "&#" ++ show (fromEnum x) ++ "; " ++
+                                       sanitize rest
+                  | otherwise        = x : sanitize rest
