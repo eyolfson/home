3226546cf24fbc538f6f6cdb7ffaf1d05383c0f2
Jon Eyolfson <jon@eyolfson.com>
2010-05-13 12:23:47 -0400

    May 13, 2010 changes.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -9,20 +9,6 @@ import XMonad.Config.Gnome
 import XMonad.Util.EZConfig (additionalKeysP)
 import XMonad.Layout.NoBorders(smartBorders)
 
--- This retry is really awkward, but sometimes DBus won't let us get our
--- name unless we retry a couple times.
-getWellKnownName :: Connection -> IO ()
-getWellKnownName dbus = tryGetName `catchDyn` (\ (DBus.Error _ _) ->
-                                                getWellKnownName dbus)
- where
-  tryGetName = do
-    namereq <- newMethodCall serviceDBus pathDBus interfaceDBus "RequestName"
-    addArgs namereq [String "org.xmonad.Log", Word32 5]
-    sendWithReplyAndBlock dbus namereq 0
-    return ()
-
-
-main :: IO ()
 main = withConnection Session $ \ dbus -> do
   getWellKnownName dbus
   xmonad $ gnomeConfig
@@ -35,28 +21,26 @@ main = withConnection Session $ \ dbus -> do
            `additionalKeysP` myKeys
 
 myPP dbus = defaultPP
-             { ppCurrent  = pangoColor "#ad7fa8" . wrap "[" "]"
-                            -- , ppHiddenNoWindows = wrap " " " "
-                            -- , ppSep = ""
-                            -- , ppWsSep = ""
-
-             , ppTitle    = pangoColor "#729fcf" . shorten 50
-             , ppVisible  = pangoColor "#663366" . wrap "(" ")"
-             , ppHidden   = wrap " " " "
-             , ppUrgent   = pangoColor "red"
-             , ppOutput   = \ str -> do
-               let str'  = "<span font=\"Terminus 9 Bold\">" ++ str ++ 
-                           "</span>"
-                   str'' = sanitize str'
-               msg <- newSignal "/org/xmonad/Log" "org.xmonad.Log" 
-                      "Update"
-               addArgs msg [String str'']
-               -- If the send fails, ignore it.
-               send dbus msg 0 `catchDyn`
-                 (\ (DBus.Error _name _msg) ->
-                   return 0)
-               return ()
-             }
+              { ppCurrent  = pangoColor "#ad7fa8" . wrap "" ""
+              --, ppHiddenNoWindows = wrap "" ""
+              --, ppSep = ""
+              --, ppWsSep = ""
+              --, ppTitle    = pangoColor "#729fcf" . shorten 50
+              , ppTitle = \x -> ""
+              , ppVisible  = pangoColor "#729fcf" . wrap "" ""
+              , ppHidden   = wrap "" ""
+              , ppUrgent   = pangoColor "#ef2929"
+              , ppOutput   = \ str -> do
+                  let str'  = "<span font=\"monospace\">" ++ str ++ "</span>"
+                      str'' = sanitize str'
+                  msg <- newSignal "/org/xmonad/Log" "org.xmonad.Log" "Update"
+                  addArgs msg [String str'']
+                  -- If the send fails, ignore it.
+                  send dbus msg 0 `catchDyn`
+                    (\ (DBus.Error _name _msg) ->
+                      return 0)
+                  return ()
+              }
 
 myKeys = [ ("M-f", spawn "firefox")
          , ("M-x", spawn "gnome-terminal")
@@ -72,12 +56,24 @@ myKeys = [ ("M-f", spawn "firefox")
 
 pangoColor :: String -> String -> String
 pangoColor fg = wrap left right
- where
-  left  = "<span foreground=\"" ++ fg ++ "\">"
-  right = "</span>"
+  where
+    left  = "<span foreground=\"" ++ fg ++ "\">"
+    right = "</span>"
 
 sanitize :: String -> String
 sanitize [] = []
 sanitize (x:rest) | fromEnum x > 127 = "&#" ++ show (fromEnum x) ++ "; " ++
                                        sanitize rest
                   | otherwise        = x : sanitize rest
+
+-- This retry is really awkward, but sometimes DBus won't let us get our
+-- name unless we retry a couple times.
+getWellKnownName :: Connection -> IO ()
+getWellKnownName dbus = tryGetName `catchDyn` (\ (DBus.Error _ _) ->
+                                                getWellKnownName dbus)
+  where
+  tryGetName = do
+    namereq <- newMethodCall serviceDBus pathDBus interfaceDBus "RequestName"
+    addArgs namereq [String "org.xmonad.Log", Word32 5]
+    sendWithReplyAndBlock dbus namereq 0
+    return ()
