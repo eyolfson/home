d3dfd6883811b778ac09eafd0b48d36095286494
Jon Eyolfson <jon@eyolfson.com>
2011-09-28 13:39:23 -0400

    Updated.

--- emacs/init.el
+++ emacs/init.el
@@ -23,15 +23,20 @@
 ;; Custom set variables
 ;;------------------------------------------------------------------------------
 (custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
  '(c-basic-offset 4)
  '(c-insert-tab-function (quote tab-to-tab-stop))
  '(c-offsets-alist (quote ((substatement . 0) (substatement-open . 0) (substatement-label . 0))))
+ '(ecb-options-version "2.40")
  '(uniquify-buffer-name-style (quote post-forward) nil (uniquify)))
 
 ;;------------------------------------------------------------------------------
 ;; ECB
 ;;------------------------------------------------------------------------------
-;;(require 'ecb)
+(require 'ecb)
 
 ;;------------------------------------------------------------------------------
 ;; Color Theme
@@ -59,3 +64,9 @@
 (require 'yasnippet)
 (yas/initialize)
 (yas/load-directory "/usr/share/emacs/site-lisp/yas/snippets")
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
