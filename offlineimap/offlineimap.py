38b02f08b81c232642b78f99e2567d652c675d0a
Jon Eyolfson <jon@eyolfson.com>
2013-08-23 23:18:27 -0400

    Added uwaterloo email account

--- offlineimap/offlineimap.py
+++ offlineimap/offlineimap.py
@@ -12,6 +12,18 @@ gmail_remote_dict = {'INBOX': 'inbox',
                      '[Gmail]/Spam': 'spam',
                      '[Gmail]/Trash': 'trash'}
 
+uwaterloo_local_dict = {'inbox': 'INBOX',
+                        'drafts': 'INBOX.Drafts',
+                        'sent': 'INBOX.Sent',
+                        'spam': 'INBOX.SPAM',
+                        'trash': 'INBOX.Trash'}
+
+uwaterloo_remote_dict = {'INBOX': 'inbox',
+                         'INBOX.Drafts': 'drafts',
+                         'INBOX.Sent': 'sent',
+                         'INBOX.SPAM': 'spam',
+                         'INBOX.Trash': 'trash'}
+
 def pass_show(account):
     o = subprocess.check_output(['pass', 'show', 'email/{}'.format(account)])
     return o.strip()
@@ -29,4 +41,14 @@ def gmail_local_nametrans(folder):
 def gmail_remote_nametrans(folder):
     if folder in gmail_remote_dict:
         return gmail_remote_dict[folder]
+    return folder
+
+def uwaterloo_local_nametrans(folder):
+    if folder in uwaterloo_local_dict:
+        return uwaterloo_local_dict[folder]
+    return folder
+
+def uwaterloo_remote_nametrans(folder):
+    if folder in uwaterloo_remote_dict:
+        return uwaterloo_remote_dict[folder]
     return folder
