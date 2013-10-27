560aa5048ae30e0eba592a913b0f32d75d57cd4b
Jon Eyolfson <jon@eyolfson.com>
2013-10-27 18:59:27 -0400

    Added outline of script

---
+++ script.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import os
+
+git_dir = os.path.dirname(os.path.abspath(__file__))
+home_dir = os.environ['HOME']
+
+if __name__ == '__main__':
+    print('\033[34mRunning script.py...\033[m')
